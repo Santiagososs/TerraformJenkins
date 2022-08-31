b52a425b94b8adadbd0d308e394389201755c5a3
Santiagososs <sheriffhermoso@gmail.com>
2022-08-30 23:17:22 -0300

    main.tf commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg-terraform" {
+  name     = "rg-terraform"
+  location = "southcentralus"
+  tags = {
+    enviroment = "dev"
+    source     = "terraform"
+    owner      = "Henry"
+  }
+}
